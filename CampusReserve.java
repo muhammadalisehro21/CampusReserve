69db87fac33bfd280aa3247fd6015d954f6ca892
Muhammad Ali Sehro <muhammadalisehro12@gmail.com>
2026-08-22 12:29:16 +0500

    Add CampusReserve class for resource reservation

--- CampusReserve.java
+++ CampusReserve.java
@@ -1,1 +1,43 @@
-CampusReserve.java
+package student;
+
+public class CampusReserve {
+    private int requestId;
+    private String userName;
+    private String resourceName;
+    private int timeSlot;
+    private int priority;
+
+    public CampusReserve(int requestId, String userName, String resourceName, int timeSlot, int priority) {
+        this.requestId = requestId;
+        this.userName = userName;
+        this.resourceName = resourceName;
+        this.timeSlot = timeSlot;
+        this.priority = priority;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public int getTimeSlot() {
+        return timeSlot;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void displayReservation() {
+        System.out.println("ID: " + requestId + " | User: " + userName + 
+                           " | Resource: " + resourceName + 
+                           " | Time: " + timeSlot + ":00 | Priority Level: " + priority);
+    }
+}
